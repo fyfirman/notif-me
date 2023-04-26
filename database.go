164eefbbb1a9514ba05f3d093572b3c56f6da1fa
Firmansyah Yanuar <fyfirman@gmail.com>
2023-04-27 00:57:08 +0700

    feat: extract to env

--- database.go
+++ database.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,7 +11,7 @@ import (
 func ConnectDB() (*gorm.DB, error) {
 	log.Println("Connecting DB...")
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := "postgres://oqknpojq:XzK6jHaPSIhovgHpuynzZ1emGJOBVYcA@john.db.elephantsql.com/oqknpojq"
+	dsn := os.Getenv("POSTGRES_URL")
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
